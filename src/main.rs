548b7ee558b89c3b52d826400382bb456abba203
s2terminal <suzuki.sh@s2terminal.com>
2022-01-15 17:39:22 +0900

    static page

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,16 @@
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
-
-struct Model {
-    value: i64,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
-    }
-}
-
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<App>();
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main>
+            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+            <h1>{ "Hello World!" }</h1>
+            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+        </main>
+    }
 }
